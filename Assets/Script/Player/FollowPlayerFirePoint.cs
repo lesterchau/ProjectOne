f5b97a0ba0b832c0a104cd69bd4cc48b4adc7457
Lester <chauchiyan1996@gmail.com>
2020-04-21 02:23:53 +0800

    Intitial commit

---
+++ Assets/Script/Player/FollowPlayerFirePoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerFirePoint : MonoBehaviour
+{
+
+    public Transform tf;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = tf.position;
+    }
+}
---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public Camera cam;
+    public Transform firePointHolder;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        firePointHolder.GetComponent<Rigidbody2D>().rotation = angle;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+
+}
