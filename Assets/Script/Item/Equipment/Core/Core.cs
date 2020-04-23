b1b7bd375f0c3a5b279bebfd58e47d716c983e0a
alvinchung01 <creamld0321@gmail.com>
2020-04-23 07:24:06 -0500

    Updated Scriptable object: Core

---
+++ Assets/Script/Item/Equipment/Core/Core.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapons", menuName = "Items/Equipments/Cores", order = 1)]
+public class Core : Equipment
+{
+    public int MaximumEnergy;
+    public int RegenerateRate;
+
+    protected new void OnValidate()
+    {
+        base.OnValidate();
+        EquapimentType = EquipmentTypes.Cores;
+    }
+}
